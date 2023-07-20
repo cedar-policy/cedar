ece56d3860e3b58dfecb53b5c3e44aae63657c0e
shaobo-he-aws <130499339+shaobo-he-aws@users.noreply.github.com>
2023-07-20 13:51:02 -0700

    Refactor `EntitiesError` (#210)

--- cedar-policy-core/src/entities/err.rs
+++ cedar-policy-core/src/entities/err.rs
@@ -16,24 +16,20 @@
 
 use super::EntityUID;
 use crate::transitive_closure;
-use std::io;
 use thiserror::Error;
 
 /// Error type for errors raised in entities.rs.
 #[derive(Debug, Error)]
 pub enum EntitiesError {
     /// Error occurring in serialization of entities
-    #[error("error while serializing entities: {0}")]
-    SerializationError(#[from] crate::entities::JsonSerializationError),
+    #[error("entities serialization error: {0}")]
+    Serialization(#[from] crate::entities::JsonSerializationError),
     /// Error occurring in deserialization of entities
-    #[error("error while deserializing entities: {0}")]
-    DeserializationError(#[from] crate::entities::JsonDeserializationError),
-    /// Error as a result of a failed IO operation.
-    #[error("IO error from the entities module: {0}")]
-    IOError(#[from] io::Error),
+    #[error("entities deserialization error: {0}")]
+    Deserialization(#[from] crate::entities::JsonDeserializationError),
     /// Errors occurring while computing or enforcing transitive closure on the
     /// entity hierarchy.
-    #[error("transitive closure error: {0}")]
+    #[error("transitive closure computation/enforcement error: {0}")]
     TransitiveClosureError(#[from] Box<transitive_closure::TcError<EntityUID>>),
 }
 
