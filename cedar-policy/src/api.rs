a7d4aa59b63432766231c44a50590766716315e0
Aaron Eline <aeline+github@amazon.com>
2023-08-07 09:49:32 -0400

    Docstring updates

--- cedar-policy/src/api.rs
+++ cedar-policy/src/api.rs
@@ -1893,6 +1893,10 @@ impl Policy {
     /// If `id` is Some, the policy will be given that Policy Id.
     /// If `id` is None, then "policy0" will be used.
     /// The behavior around None may change in the future.
+    /// 
+    /// This can fail if the policy fails to parse. 
+    /// It can also fail if a valid template was passed in, as this function only accepts Static
+    /// Policies
     pub fn parse(id: Option<String>, policy_src: impl AsRef<str>) -> Result<Self, ParseErrors> {
         let inline_ast = parser::parse_policy(id, policy_src.as_ref())?;
         let (_, ast) = ast::Template::link_static_policy(inline_ast);
