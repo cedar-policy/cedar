9d9c6c7aeba5f20edf9bce3b37efc9e505131a9f
Maxim Soloviev <max@max2.me>
2023-09-18 13:49:12 -0700

    Update err.rs

--- cedar-policy-core/src/authorizer/err.rs
+++ cedar-policy-core/src/authorizer/err.rs
@@ -22,11 +22,11 @@ use thiserror::Error;
 #[derive(Debug, PartialEq, Eq, Clone, Error)]
 pub enum AuthorizationError {
     /// Failed to eagerly evaluate entity attributes when initializing the `Evaluator`.
-    #[error("error occurred while evaluating entity attributes: {0}")]
+    #[error("Error occurred while evaluating entity attributes: {0}.")]
     AttributeEvaluationError(EvaluationError),
 
     /// An error occurred when evaluating a policy.
-    #[error("error occurred while evaluating policy `{}`: {}", &.id, &.error)]
+    #[error("Error occurred while evaluating policy '{}': {}.", &.id, &.error)]
     PolicyEvaluationError {
         /// Id of the policy with an error
         id: PolicyID,
