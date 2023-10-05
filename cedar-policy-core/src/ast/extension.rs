fe35159aa96ed935135cc1aafc8fafe55b7b7602
Andrew Wells <130512013+andrewmwells-amazon@users.noreply.github.com>
2023-10-05 11:01:05 -0700

    fix clippy (#343)

--- cedar-policy-core/src/ast/extension.rs
+++ cedar-policy-core/src/ast/extension.rs
@@ -403,7 +403,7 @@ impl Eq for ExtensionValueWithArgs {}
 
 impl PartialOrd for ExtensionValueWithArgs {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.value.partial_cmp(&other.value)
+        Some(self.cmp(other))
     }
 }
 
