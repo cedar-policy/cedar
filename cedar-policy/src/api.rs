cedar-policy/cedar

commit e5010423527e5f81496c5ac28623510ceaa559b1

Fix panic is cedar_policy::PolicySet::link (#203)
Removes an `expect` in this function which was known to panic when the
function was called with a `template_id` corresponding to a static
policy. An error result in returned instead of panicking.

Change return type of PolicySet::link to avoid an expect.
This `expect` did not panic, but this change eliminates the possibility.

cedar-policy/src/api.rs
@@ -1129,6 +1129,9 @@ pub enum PolicySetError {
     /// Expected a static policy, but a template-linked policy was provided
     #[error("expected a static policy, but a template-linked policy was provided")]
     ExpectedStatic,
+    /// Expected a template, but a static policy was provided.
+    #[error("expected a template, but a static policy was provided")]
+    ExpectedTemplate,
 }
 
 impl From<ast::PolicySetError> for PolicySetError {
@@ -1288,9 +1291,12 @@ impl PolicySet {
 
     /// Attempt to link a template and add the new template-linked policy to the policy set.
     /// If link fails, the `PolicySet` is not modified.
-    /// Failure can happen for two reasons
+    /// Failure can happen for three reasons
     ///   1) The map passed in `vals` may not match the slots in the template
     ///   2) The `new_id` may conflict w/ a policy that already exists in the set
+    ///   3) `template_id` does not correspond to a template. Either the id is
+    ///   not in the policy set, or it is in the policy set but is either a
+    ///   linked or static policy rather than a template
     #[allow(clippy::needless_pass_by_value)]
     pub fn link(
         &mut self,
@@ -1302,24 +1308,30 @@ impl PolicySet {
             .into_iter()
             .map(|(key, value)| (key.into(), value.0))
             .collect();
-        self.ast
+        let linked_ast = self
+            .ast
             .link(
                 template_id.0.clone(),
                 new_id.0.clone(),
                 unwrapped_vals.clone(),
             )
             .map_err(PolicySetError::LinkingError)?;
-        let linked_ast = self
-            .ast
-            .get(&new_id.0)
-            .expect("ast.link() didn't fail above, so this shouldn't fail");
         let linked_lossless = self
             .templates
             .get(&template_id)
-            .expect("ast.link() didn't fail above, so this shouldn't fail")
+            // We know `template_id` exists in the policy set as either a
+            // template or a static policy because otherwise `ast.link()` would
+            // have errored. If `ast.link()` did not error, then it could still
+            // be that the id corresponds to a static policy. This function
+            // should only be used to link templates, so this is an error.
+            .ok_or(PolicySetError::ExpectedTemplate)?
             .lossless
             .clone()
             .link(unwrapped_vals.iter().map(|(k, v)| (*k, v)))
+            // The only error case for `lossless.link()` is a template with
+            // slots which are not filled by the provided values. `ast.link()`
+            // will have already errored if there are any unfilled slots in the
+            // template.
             .expect("ast.link() didn't fail above, so this shouldn't fail");
         self.policies.insert(
             new_id,
@@ -2757,6 +2769,7 @@ mod head_constraints_tests {
 mod policy_set_tests {
     use super::*;
     use ast::LinkingError;
+    use cool_asserts::assert_matches;
 
     #[test]
     fn link_conflicts() {
@@ -2909,6 +2922,56 @@ mod policy_set_tests {
                 .expect("lookup failed")
                 .id(),
             &"linked".parse().unwrap()
+        );
+    }
+
+    #[test]
+    fn link_static_policy() {
+        // Linking the `PolicyId` of a static policy should not be allowed.
+        // Attempting it should cause an `ExpectedTemplate` error.
+        let static_policy = Policy::parse(
+            Some("static".into()),
+            "permit(principal, action, resource);",
+        )
+        .expect("Static parse failure");
+        let mut pset = PolicySet::new();
+        pset.add(static_policy).unwrap();
+
+        let result = pset.link(
+            PolicyId::from_str("static").unwrap(),
+            PolicyId::from_str("linked").unwrap(),
+            HashMap::new(),
+        );
+        assert_matches!(result, Err(PolicySetError::ExpectedTemplate));
+    }
+
+    #[test]
+    fn link_linked_policy() {
+        let template = Template::parse(
+            Some("template".into()),
+            "permit(principal == ?principal, action, resource);",
+        )
+        .expect("Template Parse Failure");
+        let mut pset = PolicySet::new();
+        pset.add_template(template).unwrap();
+
+        pset.link(
+            PolicyId::from_str("template").unwrap(),
+            PolicyId::from_str("linked").unwrap(),
+            std::iter::once((SlotId::principal(), EntityUid::from_strs("Test", "test"))).collect(),
+        )
+        .unwrap();
+
+        let result = pset.link(
+            PolicyId::from_str("linked").unwrap(),
+            PolicyId::from_str("linked2").unwrap(),
+            HashMap::new(),
+        );
+        assert_matches!(
+            result,
+            Err(PolicySetError::LinkingError(
+                LinkingError::NoSuchTemplate { .. }
+            ))
         );
     }
 }

cedar-policy-core/src/ast/policy_set.rs
@@ -187,7 +187,10 @@ impl PolicySet {
         }
     }
 
-    /// Attempt to create a new template linked policy and add it to the policy set
+    /// Attempt to create a new template linked policy and add it to the policy
+    /// set. Returns a references to the new template linked policy if
+    /// successful.
+    ///
     /// Errors for two reasons
     ///   1) The the passed SlotEnv either does not match the slots in the templates
     ///   2) The passed link Id conflicts with an Id already in the set
@@ -196,7 +199,7 @@ impl PolicySet {
         template_id: PolicyID,
         new_id: PolicyID,
         values: HashMap<SlotId, EntityUID>,
-    ) -> Result<(), LinkingError> {
+    ) -> Result<&Policy, LinkingError> {
         let t = self
             .get_template(&template_id)
             .ok_or_else(|| LinkingError::NoSuchTemplate {
@@ -209,10 +212,7 @@ impl PolicySet {
             self.links.entry(new_id.clone()),
             self.templates.entry(new_id),
         ) {
-            (Entry::Vacant(links_entry), Entry::Vacant(_)) => {
-                links_entry.insert(r);
-                Ok(())
-            }
+            (Entry::Vacant(links_entry), Entry::Vacant(_)) => Ok(links_entry.insert(r)),
             (Entry::Occupied(oentry), _) => Err(LinkingError::PolicyIdConflict {
                 id: oentry.key().clone(),
             }),

commit f1ab4ac967e1555551a95897b2529a6ba20e5df8

Fix `PolicySet::link` to avoid mutating policy set on link fail (#412)
Fixes bug introduced in #203

cedar-policy/src/api.rs
@@ -1972,6 +1972,24 @@ impl PolicySet {
             .into_iter()
             .map(|(key, value)| (key.into(), value.0))
             .collect();
+
+        // Try to get the template with the id we're linking from.  We do this
+        // _before_ calling `self.ast.link` because `link` mutates the policy
+        // set by creating a new link entry in a hashmap. This happens even when
+        // trying to link a static policy, which we want to error on here.
+        let template = match self.templates.get(&template_id) {
+            Some(template) => template,
+            None => {
+                return Err(if self.policies.contains_key(&template_id) {
+                    PolicySetError::ExpectedTemplate
+                } else {
+                    PolicySetError::LinkingError(ast::LinkingError::NoSuchTemplate {
+                        id: template_id.0,
+                    })
+                });
+            }
+        };
+
         let linked_ast = self
             .ast
             .link(
@@ -1980,17 +1998,10 @@ impl PolicySet {
                 unwrapped_vals.clone(),
             )
             .map_err(PolicySetError::LinkingError)?;
+
         // PANIC SAFETY: `lossless.link()` will not fail after `ast.link()` succeeds
         #[allow(clippy::expect_used)]
-        let linked_lossless = self
-            .templates
-            .get(&template_id)
-            // We know `template_id` exists in the policy set as either a
-            // template or a static policy because otherwise `ast.link()` would
-            // have errored. If `ast.link()` did not error, then it could still
-            // be that the id corresponds to a static policy. This function
-            // should only be used to link templates, so this is an error.
-            .ok_or(PolicySetError::ExpectedTemplate)?
+        let linked_lossless = template
             .lossless
             .clone()
             .link(unwrapped_vals.iter().map(|(k, v)| (*k, v)))
@@ -4073,19 +4084,23 @@ mod policy_set_tests {
         let env: HashMap<SlotId, EntityUid> =
             std::iter::once((SlotId::principal(), EntityUid::from_strs("Test", "test"))).collect();
 
+        let before_link = pset.clone();
         let r = pset.link(
             PolicyId::from_str("t").unwrap(),
             PolicyId::from_str("id").unwrap(),
             env,
         );
 
-        match r {
-            Ok(_) => panic!("Should have failed due to conflict"),
-            Err(PolicySetError::LinkingError(LinkingError::PolicyIdConflict { id })) => {
+        assert_matches!(
+            r,
+            Err(PolicySetError::LinkingError(LinkingError::PolicyIdConflict { id })) =>{
                 assert_eq!(id, ast::PolicyID::from_string("id"));
             }
-            Err(e) => panic!("Incorrect error: {e}"),
-        };
+        );
+        assert_eq!(
+            pset, before_link,
+            "A failed link shouldn't mutate the policy set"
+        );
     }
 
     #[test]
@@ -4374,12 +4389,17 @@ mod policy_set_tests {
         let mut pset = PolicySet::new();
         pset.add(static_policy).unwrap();
 
+        let before_link = pset.clone();
         let result = pset.link(
             PolicyId::from_str("static").unwrap(),
             PolicyId::from_str("linked").unwrap(),
             HashMap::new(),
         );
         assert_matches!(result, Err(PolicySetError::ExpectedTemplate));
+        assert_eq!(
+            pset, before_link,
+            "A failed link shouldn't mutate the policy set"
+        );
     }
 
     #[test]
@@ -4399,16 +4419,16 @@ mod policy_set_tests {
         )
         .unwrap();
 
+        let before_link = pset.clone();
         let result = pset.link(
             PolicyId::from_str("linked").unwrap(),
             PolicyId::from_str("linked2").unwrap(),
             HashMap::new(),
         );
-        assert_matches!(
-            result,
-            Err(PolicySetError::LinkingError(
-                LinkingError::NoSuchTemplate { .. }
-            ))
+        assert_matches!(result, Err(PolicySetError::ExpectedTemplate));
+        assert_eq!(
+            pset, before_link,
+            "A failed link shouldn't mutate the policy set"
         );
     }
 