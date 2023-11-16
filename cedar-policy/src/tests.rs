933a4adaa605eef90cb2dc0e53db59269a6f5e6c
Andrew Wells <anmwells@amazon.com>
2023-11-16 23:18:49 +0000

    break test

--- cedar-policy/src/tests.rs
+++ cedar-policy/src/tests.rs
@@ -2718,7 +2718,7 @@ mod schema_based_parsing_tests {
         );
         assert_matches!(
             parsed.attr("json_blob"),
-            Some(Err(e)) => assert_contains_unknown(&e.to_string(), "bbb")
+            Some(Err(e)) => assert_contains_unknown(&e.to_string(), "bbbb")
         );
         assert_matches!(
             parsed.attr("home_ip"),
