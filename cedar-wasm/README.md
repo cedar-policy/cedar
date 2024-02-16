5a1338ceba027aeea4350ea821f6a535f2540c03
John Kastner <jkastner@amazon.com>
2024-02-16 20:00:43 +0000

    Revert "v0 of cedar-wasm (#566)"
    This reverts commit 25c4229298595cc9c39c47a3260518205823ce41.

--- cedar-wasm/README.md
+++
@@ -1,3 +0,0 @@
-# cedar-wasm
-
-An implementation of various cedar functions to enable developers to write typescript and javascript applications using Cedar and wasm.
--- cedar-wasm/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![forbid(unsafe_code)]
-
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen(js_name = "getCedarVersion")]
-pub fn get_cedar_version() -> String {
-    std::env!("CEDAR_VERSION").to_string()
-}
