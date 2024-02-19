be509115776790cbcf2e60b33bed44403510efb6
Shaobo He <shaobohe@amazon.com>
2024-02-19 11:51:45 -0800

    tried again to fix CI failure

--- cedar-policy-validator/src/human_schema/ast.rs
+++ cedar-policy-validator/src/human_schema/ast.rs
@@ -7,6 +7,7 @@ use cedar_policy_core::{
 use itertools::Itertools;
 use nonempty::NonEmpty;
 use smol_str::SmolStr;
+#[allow(unused_imports)]
 use smol_str::ToSmolStr;
 
 use crate::SchemaTypeVariant;
