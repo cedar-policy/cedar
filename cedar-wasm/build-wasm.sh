244486c56c74ba76c247d6146e2b6a56cd4740af
Nicholas Szegheo <nicholas.szegheo@gmail.com>
2024-03-05 14:48:58 -0500

    tsify ValidateCall and schema types for cedar-wasm, add wasm build to CI (#692)
    Signed-off-by: Nick Szegheo <nicholas.szegheo@gmail.com>
    Signed-off-by: Kesha Hietala <khieta@amazon.com>

--- cedar-wasm/build-wasm.sh
+++ cedar-wasm/build-wasm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script calls wasm-pack build and post-processes the generated TS types to fix them.
+# Without this, the built wasm still works, but the Typescript definitions made by tsify don't.
 set -e
 cargo build
 wasm-pack build --scope amzn --target web
@@ -17,6 +19,8 @@ sed -i "s/[{]\s*-: /{ \"-\": /g" pkg/cedar_wasm.d.ts
 sed -i "s/[{]\s*[*]: /{ \"*\": /g" pkg/cedar_wasm.d.ts
 sed -i "s/[{]\s*\.: /{ \".\": /g" pkg/cedar_wasm.d.ts
 sed -i "s/ | __skip//g" pkg/cedar_wasm.d.ts
+sed -i "s/SchemaFragment/Schema/g" pkg/cedar_wasm.d.ts
 
 echo "type SmolStr = string;" >> pkg/cedar_wasm.d.ts
+echo "export type TypeOfAttribute = SchemaType & { required?: boolean };" >> pkg/cedar_wasm.d.ts
 echo "Finished post-processing types file"
