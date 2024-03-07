5ed3ff95fbe35141c9f1c9e042f26f3b8336f3df
John Kastner <130772734+john-h-kastner-aws@users.noreply.github.com>
2024-03-07 13:26:43 -0500

    Improve parser errors on unexpected tokens (#698)
    Signed-off-by: John Kastner <jkastner@amazon.com>

--- cedar-policy-core/src/parser.rs
+++ cedar-policy-core/src/parser.rs
@@ -689,6 +689,7 @@ mod parse_tests {
     use super::*;
     use crate::test_utils::*;
     use cool_asserts::assert_matches;
+    use itertools::Itertools;
     use miette::Diagnostic;
 
     #[test]
@@ -1058,5 +1059,70 @@ mod parse_tests {
                 itertools::assert_equal(err.labels().expect("should have labels"), [miette::LabeledSpan::underline(expected_span)]);
             }
         })
+    }
+
+    #[test]
+    fn unexpected_token_errors() {
+        #[track_caller]
+        fn assert_labeled_span(policy: &str, label: impl Into<String>) {
+            assert_matches!(parse_policy(None, policy), Err(e) => {
+                let actual_label = e.labels().and_then(|l| {
+                    l.exactly_one()
+                        .ok()
+                        .expect("Assumed that there would be exactly one label if labels are present")
+                        .label()
+                        .map(|l| l.to_string())
+                });
+                assert_eq!(Some(label.into()), actual_label, "Did not see expected labeled span.");
+            });
+        }
+
+        // Don't list out all the special case identifiers
+        assert_labeled_span("@", "expected identifier");
+        assert_labeled_span(
+            "permit(principal, action, resource) when { principal.",
+            "expected identifier",
+        );
+
+        // We specifically want `when` or `unless`, but we previously listed all
+        // identifier tokens, so this is an improvement.
+        assert_labeled_span(
+            "permit(principal, action, resource)",
+            "expected `;` or identifier",
+        );
+        // AST actually requires `permit` or `forbid`, but grammar looks for any
+        // identifier.
+        assert_labeled_span("@if(\"a\")", "expected `@` or identifier");
+        // AST actually requires `principal` (`action`, `resource`, resp.). In
+        // the `principal` case we also claim to expect `)` because an empty scope
+        // initially parses to a CST. The trailing comma rules this out in the others.
+        assert_labeled_span("permit(", "expected `)` or identifier");
+        assert_labeled_span("permit(,,);", "expected `)` or identifier");
+        assert_labeled_span("permit(principal,", "expected identifier");
+        assert_labeled_span("permit(principal,action,", "expected identifier");
+        // Nothing will actually convert to an AST here.
+        assert_labeled_span("permit(principal,action,resource,", "expected identifier");
+        // We still list out `if` as an expected token because it doesn't get
+        // parsed as an ident in this position.
+        assert_labeled_span(
+            "permit(principal, action, resource) when {",
+            "expected `!`, `(`, `-`, `[`, `{`, `}`, `false`, identifier, `if`, number, `?principal`, `?resource`, string literal, or `true`",
+        );
+        // The right operand of an `is` gets parsed as any `Expr`, so we will
+        // list out all the possible expression tokens even though _only_
+        // `identifier` is accepted. This choice allows nicer error messages for
+        // `principal is User::"alice"`, but it doesn't work in our favor here.
+        assert_labeled_span(
+            "permit(principal, action, resource) when { principal is",
+            "expected `!`, `(`, `-`, `[`, `{`, `false`, identifier, `if`, number, `?principal`, `?resource`, string literal, or `true`",
+        );
+
+        // We expect binary operators, but don't claim to expect `=`, `%` or
+        // `/`. We still expect `::` even though `true` is a reserved identifier
+        // and so we can't have an entity reference `true::"eid"`
+        assert_labeled_span(
+            "permit(principal, action, resource) when { if true",
+            "expected `!=`, `&&`, `(`, `*`, `+`, `-`, `.`, `::`, `<`, `<=`, `==`, `>`, `>=`, `[`, `||`, `has`, `in`, `is`, `like`, or `then`",
+        )
     }
 }
