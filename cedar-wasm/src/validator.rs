ea00969cd5eb987316e152d4599cf69827271194
Nicholas Szegheo <nicholas.szegheo@gmail.com>
2024-04-02 13:26:48 -0400

    Make non-json isAuthorized and validate public, rework wasm interfaces (#737)
    Signed-off-by: Nick Szegheo <nicholas.szegheo@gmail.com>

--- cedar-wasm/src/validator.rs
+++ cedar-wasm/src/validator.rs
@@ -1,7 +1,34 @@
-use cedar_policy::frontend::{utils::InterfaceResult, validate::json_validate};
+use cedar_policy::frontend::validate::{
+    validate, ValidationAnswer, ValidationCall, ValidationError, ValidationWarning,
+};
+use serde::{Deserialize, Serialize};
+use tsify::Tsify;
 use wasm_bindgen::prelude::*;
 
+#[derive(Tsify, Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub enum ValidateResult {
+    #[serde(rename_all = "camelCase")]
+    Success {
+        validation_errors: Vec<ValidationError>,
+        validation_warnings: Vec<ValidationWarning>,
+    },
+    Error {
+        errors: Vec<String>,
+    },
+}
+
 #[wasm_bindgen(js_name = "validate")]
-pub fn wasm_validate(input: &str) -> InterfaceResult {
-    json_validate(input)
+pub fn wasm_validate(call: ValidationCall) -> ValidateResult {
+    match validate(call) {
+        ValidationAnswer::Success {
+            validation_errors,
+            validation_warnings,
+        } => ValidateResult::Success {
+            validation_errors,
+            validation_warnings,
+        },
+        ValidationAnswer::Failure { errors } => ValidateResult::Error { errors },
+    }
 }
