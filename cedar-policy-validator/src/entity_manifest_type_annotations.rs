ff64d4a5e9a46bac97631dd0a5a79f824d6a18ff
oflatt <oflatt@gmail.com>
2024-09-10 17:06:38 -0700

    progress on error messages
    Signed-off-by: oflatt <oflatt@gmail.com>

--- cedar-policy-validator/src/entity_manifest_type_annotations.rs
+++ cedar-policy-validator/src/entity_manifest_type_annotations.rs
@@ -21,7 +21,11 @@ use std::collections::HashMap;
 use cedar_policy_core::ast::{RequestType, Var};
 
 use crate::{
-    entity_manifest::{AccessTrie, EntityManifest, EntityRoot, Fields, RootAccessTrie},
+    entity_manifest::{
+        AccessTrie, EntityManifest, EntityRoot, Fields, MismatchedEntityManifestError,
+        MismatchedMissingAttributeError, MismatchedMissingEntityError,
+        MismatchedNotStrictSchemaError, RootAccessTrie,
+    },
     types::{Attributes, EntityRecordKind, Type},
     ValidatorSchema,
 };
@@ -29,14 +33,22 @@ use crate::{
 impl EntityManifest {
     /// Given an untyped entity manifest and the schema that produced it,
     /// return a newly typed entity manifest.
-    pub(crate) fn to_typed(&self, schema: &ValidatorSchema) -> EntityManifest {
-        EntityManifest {
-            per_action: self
-                .per_action
-                .iter()
-                .map(|(key, val)| (key.clone(), val.to_typed(key, schema)))
-                .collect(),
-        }
+    pub(crate) fn to_typed(
+        &self,
+        schema: &ValidatorSchema,
+    ) -> Result<EntityManifest, MismatchedEntityManifestError> {
+        Ok(
+            EntityManifest {
+                per_action:
+                    self.per_action
+                        .iter()
+                        .map(|(key, val)| Ok((key.clone(), val.to_typed(key, schema)?)))
+                        .collect::<Result<
+                            HashMap<RequestType, RootAccessTrie>,
+                            MismatchedEntityManifestError,
+                        >>()?,
+            },
+        )
     }
 }
 
@@ -47,48 +59,56 @@ impl RootAccessTrie {
         &self,
         request_type: &RequestType,
         schema: &ValidatorSchema,
-    ) -> RootAccessTrie {
-        RootAccessTrie {
+    ) -> Result<RootAccessTrie, MismatchedEntityManifestError> {
+        Ok(RootAccessTrie {
             trie: self
                 .trie
                 .iter()
                 .map(|(key, slice)| {
-                    (
+                    Ok((
                         key.clone(),
                         match key {
-                            // PANIC SAFETY: literal is checked against the schema during typechecking
-                            #[allow(clippy::unwrap_used)]
                             EntityRoot::Literal(lit) => slice.to_typed(
                                 request_type,
-                                &Type::euid_literal(lit.clone(), schema).unwrap(),
+                                &Type::euid_literal(lit.clone(), schema).ok_or(
+                                    MismatchedMissingEntityError {
+                                        entity: lit.clone(),
+                                    },
+                                )?,
                                 schema,
-                            ),
-                            // PANIC SAFETY: action literal is checked against the schema during typechecking
+                            )?,
                             EntityRoot::Var(Var::Action) => {
                                 let ty = Type::euid_literal(request_type.action.clone(), schema)
-                                    .unwrap();
-                                slice.to_typed(request_type, &ty, schema)
+                                    .ok_or(MismatchedMissingEntityError {
+                                        entity: request_type.action.clone(),
+                                    })?;
+                                slice.to_typed(request_type, &ty, schema)?
                             }
                             EntityRoot::Var(Var::Principal) => slice.to_typed(
                                 request_type,
                                 &Type::named_entity_reference(request_type.principal.clone()),
                                 schema,
-                            ),
+                            )?,
                             EntityRoot::Var(Var::Resource) => slice.to_typed(
                                 request_type,
                                 &Type::named_entity_reference(request_type.resource.clone()),
                                 schema,
-                            ),
+                            )?,
                             EntityRoot::Var(Var::Context) => {
-                                let ty =
-                                    &schema.get_action_id(&request_type.action).unwrap().context;
-                                slice.to_typed(request_type, ty, schema)
+                                let ty = &schema
+                                    .get_action_id(&request_type.action.clone())
+                                    .ok_or(MismatchedMissingEntityError {
+                                        entity: request_type.action.clone(),
+                                    })?
+                                    .context;
+                                slice.to_typed(request_type, ty, schema)?
                             }
                         },
-                    )
+                    ))
                 })
-                .collect(),
-        }
+                .collect::<Result<HashMap<EntityRoot, AccessTrie>, MismatchedEntityManifestError>>(
+                )?,
+        })
     }
 }
 
@@ -98,55 +118,62 @@ impl AccessTrie {
         request_type: &RequestType,
         ty: &Type,
         schema: &ValidatorSchema,
-    ) -> AccessTrie {
-        let children: Fields = match ty {
-            Type::Never
-            | Type::True
-            | Type::False
-            | Type::Primitive { .. }
-            | Type::Set { .. }
-            | Type::ExtensionType { .. } => {
-                assert!(self.children.len() == 0);
-                HashMap::default()
-            }
-            Type::EntityOrRecord(entity_or_record_ty) => {
-                let attributes: &Attributes = match entity_or_record_ty {
-                    EntityRecordKind::Record {
-                        attrs,
-                        open_attributes: _,
-                    } => attrs,
-                    // PANIC SAFETY: strict validation shouldn't produce the AnyEntity type
-                    #[allow(clippy::panic)]
-                    EntityRecordKind::AnyEntity => {
-                        panic!("Strict validation resulted in AnyEntity")
-                    }
-                    EntityRecordKind::Entity(entitylub) => {
-                        let entity_ty = schema
-                            .get_entity_type(entitylub.get_single_entity().unwrap())
-                            .unwrap();
-                        &entity_ty.attributes
-                    }
-                    EntityRecordKind::ActionEntity { name: _, attrs } => attrs,
-                };
+    ) -> Result<AccessTrie, MismatchedEntityManifestError> {
+        let children: Fields =
+            match ty {
+                Type::Never
+                | Type::True
+                | Type::False
+                | Type::Primitive { .. }
+                | Type::Set { .. }
+                | Type::ExtensionType { .. } => {
+                    assert!(self.children.is_empty());
+                    HashMap::default()
+                }
+                Type::EntityOrRecord(entity_or_record_ty) => {
+                    let attributes: &Attributes = match entity_or_record_ty {
+                        EntityRecordKind::Record {
+                            attrs,
+                            open_attributes: _,
+                        } => attrs,
+                        EntityRecordKind::AnyEntity => Err(MismatchedNotStrictSchemaError {})?,
+                        // PANIC SAFETY: entity LUB should succeed after strict validation, and so should looking up the resulting type
+                        #[allow(clippy::unwrap_used)]
+                        EntityRecordKind::Entity(entitylub) => {
+                            let entity_ty = schema
+                                .get_entity_type(
+                                    entitylub
+                                        .get_single_entity()
+                                        .ok_or(MismatchedNotStrictSchemaError {})?,
+                                )
+                                .ok_or(MismatchedNotStrictSchemaError {})?;
+                            &entity_ty.attributes
+                        }
+                        EntityRecordKind::ActionEntity { name: _, attrs } => attrs,
+                    };
 
-                self.children
-                    .iter()
-                    .map(|(field, child)| {
-                        let ty = attributes.attrs.get(field).unwrap();
-                        (
-                            field.clone(),
-                            Box::new(child.to_typed(&request_type, &ty.attr_type, schema)),
-                        )
-                    })
-                    .collect()
-            }
-        };
+                    self.children
+                        .iter()
+                        .map(|(field, child)| {
+                            let ty = attributes.attrs.get(field).ok_or(
+                                MismatchedMissingAttributeError {
+                                    field: field.clone(),
+                                },
+                            )?;
+                            Ok((
+                                field.clone(),
+                                Box::new(child.to_typed(request_type, &ty.attr_type, schema)?),
+                            ))
+                        })
+                        .collect::<Result<Fields, MismatchedEntityManifestError>>()?
+                }
+            };
 
-        AccessTrie {
+        Ok(AccessTrie {
             children,
             node_type: Some(ty.clone()),
-            ancestors_trie: self.ancestors_trie.to_typed(request_type, schema),
+            ancestors_trie: self.ancestors_trie.to_typed(request_type, schema)?,
             is_ancestor: self.is_ancestor,
-        }
+        })
     }
 }
