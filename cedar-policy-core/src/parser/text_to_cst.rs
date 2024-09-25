3a66136d0600fe42f333ca257a02be97c2ca6813
John Kastner <jkastner@amazon.com>
2024-09-25 14:31:49 +0000

    another test
    Signed-off-by: John Kastner <jkastner@amazon.com>

--- cedar-policy-core/src/parser/text_to_cst.rs
+++ cedar-policy-core/src/parser/text_to_cst.rs
@@ -1035,6 +1035,24 @@ mod tests {
     }
 
     #[test]
+    fn policy_annotations_no_value_ok() {
+        let policy = assert_parse_succeeds(
+            parse_policy,
+            r#"@foo permit (principal, action, resource); "#,
+        );
+        let annotation = policy.annotations.first().unwrap().as_inner().unwrap();
+
+        // The CST distinguishes the between `None` and empty string case, so we
+        // test this in addition to no-value tests on the AST.
+        assert_eq!(annotation.value, None);
+        assert_eq!(
+            annotation.key.as_inner().unwrap().to_string(),
+            "foo".to_string()
+        );
+        assert_eq!(policy.annotations.len(), 1);
+    }
+
+    #[test]
     fn policy_annotations_bad_id() {
         let src = r#"
             @bad-annotation("bad") permit (principal, action, resource);
