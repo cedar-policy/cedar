d17a414e1f12219e5610ea28f7ce6f4a4ffbbc9d
Craig Disselkoen <cdiss@amazon.com>
2025-01-06 10:18:55 -0500

    fix educe bounds
    Signed-off-by: Craig Disselkoen <cdiss@amazon.com>

--- cedar-policy-validator/src/json_schema.rs
+++ cedar-policy-validator/src/json_schema.rs
@@ -983,7 +983,7 @@ impl From<EntityUID> for ActionEntityUID<Name> {
 /// this [`Type`], including recursively.
 /// See notes on [`Fragment`].
 #[derive(Educe, Debug, Clone, Serialize)]
-#[educe(PartialEq, Eq, PartialOrd, Ord)]
+#[educe(PartialEq(bound(N: PartialEq)), Eq, PartialOrd(bound(N: PartialOrd)), Ord(bound(N: Ord)))]
 // This enum is `untagged` with these variants as a workaround to a serde
 // limitation. It is not possible to have the known variants on one enum, and
 // then, have catch-all variant for any unrecognized tag in the same enum that
@@ -1603,7 +1603,7 @@ impl RecordType<ConditionalName> {
 /// this [`TypeVariant`], including recursively.
 /// See notes on [`Fragment`].
 #[derive(Educe, Debug, Clone, Serialize, Deserialize)]
-#[educe(PartialEq(bound(N: PartialEq)), Eq, PartialOrd, Ord)]
+#[educe(PartialEq(bound(N: PartialEq)), Eq, PartialOrd(bound(N: PartialOrd)), Ord(bound(N: Ord)))]
 #[serde(tag = "type")]
 #[serde(bound(deserialize = "N: Deserialize<'de> + From<RawName>"))]
 #[cfg_attr(feature = "wasm", derive(tsify::Tsify))]
@@ -3590,5 +3590,20 @@ mod annotations {
            }
         });
         test_unknown_fields(src, "`bar`", ATTRIBUTE_TYPE_EXPECTED_ATTRIBUTES);
+    }
+}
+
+#[cfg(test)]
+mod ord {
+    use super::{Type, TypeVariant, RawName, InternalName};
+    use std::collections::BTreeSet;
+
+    /// Tests that `Type<RawName>` and `Type<InternalName>` are `Ord`
+    #[test]
+    fn type_ord() {
+        let mut set: BTreeSet<Type<RawName>> = BTreeSet::default();
+        set.insert(Type::Type { ty: TypeVariant::String, loc: None });
+        let mut set: BTreeSet<Type<InternalName>> = BTreeSet::default();
+        set.insert(Type::Type { ty: TypeVariant::String, loc: None });
     }
 }
