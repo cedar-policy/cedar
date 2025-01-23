8d662dd92ebcc3439ead00ac40332ef2df229f14
Shaobo He <shaobohe@amazon.com>
2025-01-23 10:31:20 -0800

    ok
    Signed-off-by: Shaobo He <shaobohe@amazon.com>

--- cedar-policy/src/tests.rs
+++ cedar-policy/src/tests.rs
@@ -6287,7 +6287,7 @@ mod version_tests {
 
     #[test]
     fn test_sdk_version() {
-        assert_eq!(get_sdk_version().to_string(), "4.3.0");
+        assert_eq!(get_sdk_version().to_string(), "4.3.1");
     }
 
     #[test]
