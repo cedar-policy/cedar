f030b1131aaa3528f9dd4fa1c20a1ed15626224f
Jonathan Eidelman <jeid@amazon.com>
2025-02-18 11:36:39 -0500

    Addressed comments and removed panics - moved error stuff to a feature
    Signed-off-by: Jonathan Eidelman <jeid@amazon.com>

--- cedar-policy-core/src/parser/cst_to_ast.rs
+++ cedar-policy-core/src/parser/cst_to_ast.rs
@@ -2385,16 +2385,6 @@ mod tests {
     }
 
     #[track_caller]
-    fn assert_parse_policy_allows_errors(text: &str) -> ast::StaticPolicy {
-        text_to_cst::parse_policy(text)
-            .expect("failed parser")
-            .to_policy_with_errors(ast::PolicyID::from_string("id"))
-            .unwrap_or_else(|errs| {
-                panic!("failed conversion to AST:\n{:?}", miette::Report::new(errs))
-            })
-    }
-
-    #[track_caller]
     fn assert_parse_policy_fails(text: &str) -> ParseErrors {
         let result = text_to_cst::parse_policy(text)
             .expect("failed parser")
@@ -2405,14 +2395,6 @@ mod tests {
             }
             Err(errs) => errs,
         }
-    }
-
-    #[test]
-    fn parsing_with_errors_succeeds_with_empty_when() {
-        let src = r#"
-            permit(principal, action, resource) when {};
-        "#;
-        assert_parse_policy_allows_errors(src);
     }
 
     #[test]
