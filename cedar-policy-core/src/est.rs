d45abdbef7dc48558f6a2b74b649288ed84994f0
Jonathan Eidelman <jeid@amazon.com>
2025-02-25 09:32:45 -0500

    Add concept of tolerant parser and Policy Error node in the CST
    Signed-off-by: Jonathan Eidelman <jeid@amazon.com>

--- cedar-policy-core/src/est.rs
+++ cedar-policy-core/src/est.rs
@@ -31,7 +31,6 @@ use crate::ast::EntityUID;
 use crate::ast::{self, Annotation};
 use crate::entities::json::{err::JsonDeserializationError, EntityUidJson};
 use crate::expr_builder::ExprBuilder;
-use crate::parser::cst;
 use crate::parser::err::{parse_errors, ParseErrors, ToASTError, ToASTErrorKind};
 use crate::parser::util::{flatten_tuple_2, flatten_tuple_4};
 #[cfg(feature = "tolerant-ast")]
