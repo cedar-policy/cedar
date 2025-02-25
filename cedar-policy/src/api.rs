741eb11a41eaf11500a920ad0c87f1ca031611e9
Jonathan Eidelman <jeid@amazon.com>
2025-02-25 14:33:43 -0500

    Remove changes to API
    Signed-off-by: Jonathan Eidelman <jeid@amazon.com>

--- cedar-policy/src/api.rs
+++ cedar-policy/src/api.rs
@@ -3050,9 +3050,6 @@ pub enum ActionConstraint {
     In(Vec<EntityUid>),
     /// Must be equal to the given [`EntityUid]`
     Eq(EntityUid),
-    #[cfg(feature = "tolerant-ast")]
-    /// Represents an action constraint that failed to parse
-    ErrorConstraint,
 }
 
 /// Scope constraint on policy resources.
