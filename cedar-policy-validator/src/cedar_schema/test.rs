6187f6ca6aff80b58f2b5ee31f28556b22fa4718
Jonathan Eidelman <jeid@amazon.com>
2025-03-17 15:41:15 -0400

    More changes
    Signed-off-by: Jonathan Eidelman <jeid@amazon.com>

--- cedar-policy-validator/src/cedar_schema/test.rs
+++ cedar-policy-validator/src/cedar_schema/test.rs
@@ -2517,6 +2517,8 @@ mod common_type_references {
             AttributeType {
                 attr_type: Type::EntityOrRecord(EntityRecordKind::Record { attrs, open_attributes: _ }),
                 is_required: true,
+                #[cfg(feature = "extended-schema")]
+                loc: None
             } => {
                 assert_eq!(attrs.get_attr("a").unwrap().attr_type, Type::primitive_long());
             }
@@ -2544,6 +2546,8 @@ mod common_type_references {
             AttributeType {
                 attr_type: Type::EntityOrRecord(EntityRecordKind::Record { attrs, open_attributes: _ }),
                 is_required: true,
+                #[cfg(feature = "extended-schema")]
+                loc: None
             } => {
                 assert_eq!(attrs.get_attr("a").unwrap().attr_type, Type::primitive_long());
             }
@@ -2575,6 +2579,8 @@ mod common_type_references {
             AttributeType {
                 attr_type: Type::EntityOrRecord(EntityRecordKind::Record { attrs, open_attributes: _ }),
                 is_required: true,
+                #[cfg(feature = "extended-schema")]
+                loc: None
             } => {
                 assert_eq!(attrs.get_attr("a").unwrap().attr_type, Type::set(Type::primitive_long()));
             }
