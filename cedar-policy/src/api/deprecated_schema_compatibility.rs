ac1c412d0ad85d778d6e2074a2c6399030c8601b
John Kastner <jkastner@amazon.com>
2025-05-02 18:19:18 +0000

    tests
    Signed-off-by: John Kastner <jkastner@amazon.com>

--- cedar-policy/src/api/deprecated_schema_compatibility.rs
+++ cedar-policy/src/api/deprecated_schema_compatibility.rs
@@ -90,28 +90,21 @@ impl Schema {
     }
 }
 
+/// These tests assert that specific unknown fields are allowed in the
+/// compatibility mode, but not allowed in the standard schema parsing mode.
 #[cfg(test)]
 #[allow(deprecated)]
-mod test {
+mod extra_fields_allowed {
     use cedar_policy_core::test_utils::{expect_err, ExpectedErrorMessageBuilder};
     use miette::Report;
     use serde_json::json;
 
     use crate::Schema;
 
-    #[track_caller]
-    fn assert_ok_deprecated_and_err_standard(schema: serde_json::Value, err: &str) {
-        Schema::from_deprecated_json_value(schema.clone()).unwrap();
-        expect_err(
-            "",
-            &Report::new(Schema::from_json_value(schema.clone()).unwrap_err()),
-            &ExpectedErrorMessageBuilder::error(err).build(),
-        );
-    }
-
-    fn schema_with_attribute(attr_ty: serde_json::Value) -> serde_json::Value {
+    fn schema_with_entity_attribute(attr_ty: serde_json::Value) -> serde_json::Value {
         json!({
             "ns": {
+                "commonTypes": {"ty": {"type": "Long"}},
                 "entityTypes": {
                     "User": {
                         "shape": {
@@ -127,136 +120,346 @@ mod test {
         })
     }
 
+    fn schema_with_entity_tag(attr_ty: serde_json::Value) -> serde_json::Value {
+        json!({
+            "ns": {
+                "commonTypes": {"ty": {"type": "Long"}},
+                "entityTypes": {
+                    "User": { "tags": attr_ty, }
+                },
+                "actions": {},
+            }
+        })
+    }
+
+    fn schema_with_context_attribute(attr_ty: serde_json::Value) -> serde_json::Value {
+        json!({
+            "ns": {
+                "commonTypes": {"ty": {"type": "Long"}},
+                "entityTypes": {
+                    "User": {}
+                },
+                "actions": {
+                    "Act": {
+                        "appliesTo": {
+                            "principalTypes": [],
+                            "resourceTypes": [],
+                            "context": {
+                                "type": "Record",
+                                "attributes": {
+                                    "foo": attr_ty,
+                                },
+                            }
+                        }
+                    }
+                },
+            }
+        })
+    }
+
+    fn schema_with_common_type(ty: serde_json::Value) -> serde_json::Value {
+        json!({
+            "ns": {
+                "commonTypes": {
+                    "ty": {"type": "Long"},
+                    "ty2": ty,
+                },
+                "entityTypes": {
+                    "User": {}
+                },
+                "actions": {},
+            }
+        })
+    }
+
+    #[track_caller]
+    fn assert_ok_deprecated_and_err_standard(ty: serde_json::Value, err: &str) {
+        let in_entity_attr = schema_with_entity_attribute(ty.clone());
+        Schema::from_deprecated_json_value(in_entity_attr.clone()).unwrap();
+        expect_err(
+            "",
+            &Report::new(Schema::from_json_value(in_entity_attr).unwrap_err()),
+            &ExpectedErrorMessageBuilder::error(err).build(),
+        );
+        let in_entity_tag = schema_with_entity_tag(ty.clone());
+        Schema::from_deprecated_json_value(in_entity_tag.clone()).unwrap();
+        expect_err(
+            "",
+            &Report::new(Schema::from_json_value(in_entity_tag).unwrap_err()),
+            &ExpectedErrorMessageBuilder::error(err).build(),
+        );
+        let in_context = schema_with_context_attribute(ty.clone());
+        Schema::from_deprecated_json_value(in_context.clone()).unwrap();
+        expect_err(
+            "",
+            &Report::new(Schema::from_json_value(in_context).unwrap_err()),
+            &ExpectedErrorMessageBuilder::error(err).build(),
+        );
+        let in_common = schema_with_common_type(ty.clone());
+        Schema::from_deprecated_json_value(in_common.clone()).unwrap();
+        expect_err(
+            "",
+            &Report::new(Schema::from_json_value(in_common).unwrap_err()),
+            &ExpectedErrorMessageBuilder::error(err).build(),
+        );
+    }
+
     #[test]
-    fn extra_fields_in_long() {
+    fn in_long() {
         assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
+            json!({
                 "type": "Long",
                 "name": "my_long",
-            })),
+            }),
             "unknown field `name`, there are no fields",
         );
         assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
+            json!({
                 "type": "Long",
-                "element": "thing",
-            })),
+                "element": {"type": "Long"},
+            }),
             "unknown field `element`, there are no fields",
         );
         assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
+            json!({
                 "type": "Long",
-                "attributes": "bar",
-            })),
+                "attributes": {},
+            }),
             "unknown field `attributes`, there are no fields",
         );
         assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
+            json!({
                 "type": "Long",
-                "additional_attributes": "bar",
-            })),
-            "unknown field `additional_attributes`, there are no fields",
+                "additionalAttributes": false,
+            }),
+            "unknown field `additionalAttributes`, there are no fields",
         );
     }
 
     #[test]
-    fn extra_fields_in_bool() {
+    fn in_bool() {
         assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
+            json!({
                 "type": "Boolean",
                 "name": "my_long",
-            })),
-            " unknown field `name`, there are no fields",
-        );
-        assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
-                "type": "Boolean",
-                "element": "thing",
-            })),
-            " unknown field `element`, there are no fields",
-        );
-        assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
-                "type": "Boolean",
-                "attributes": "bar",
-            })),
-            " unknown field `attributes`, there are no fields",
-        );
-        assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
-                "type": "Boolean",
-                "additional_attributes": "bar",
-            })),
-            "unknown field `additional_attributes`, there are no fields",
-        );
-    }
-
-    #[test]
-    fn extra_fields_in_string() {
-        assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
-                "type": "String",
-                "name": "my_long",
-            })),
+            }),
             "unknown field `name`, there are no fields",
         );
         assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
-                "type": "String",
-                "element": "thing",
-            })),
+            json!({
+                "type": "Boolean",
+                "element": {"type": "Long"},
+            }),
             "unknown field `element`, there are no fields",
         );
         assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
-                "type": "String",
-                "attributes": "bar",
-            })),
+            json!({
+                "type": "Boolean",
+                "attributes": {},
+            }),
             "unknown field `attributes`, there are no fields",
         );
         assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
-                "type": "String",
-                "additional_attributes": "bar",
-            })),
-            "unknown field `additional_attributes`, there are no fields",
+            json!({
+                "type": "Boolean",
+                "additionalAttributes": false,
+            }),
+            "unknown field `additionalAttributes`, there are no fields",
         );
     }
 
     #[test]
-    fn extra_fields_in_set() {
+    fn in_string() {
         assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
+            json!({
+                "type": "String",
+                "name": "my_long",
+            }),
+            "unknown field `name`, there are no fields",
+        );
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "String",
+                "element": {"type": "Long"},
+            }),
+            "unknown field `element`, there are no fields",
+        );
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "String",
+                "attributes": {},
+            }),
+            "unknown field `attributes`, there are no fields",
+        );
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "String",
+                "additionalAttributes": false,
+            }),
+            "unknown field `additionalAttributes`, there are no fields",
+        );
+    }
+
+    #[test]
+    fn in_set() {
+        assert_ok_deprecated_and_err_standard(
+            json!({
                 "type": "Set",
                 "element": {"type": "Long"},
                 "name": "my_long",
-            })),
-            "unknown field `name`, there are no fields",
+            }),
+            "unknown field `name`, expected `element`",
         );
         assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
+            json!({
                 "type": "Set",
                 "element": {"type": "Long", "name": "my_long"},
-            })),
+            }),
             "unknown field `name`, there are no fields",
         );
         assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
-                "type": "Long",
+            json!({
+                "type": "Set",
                 "element": {"type": "Long"},
-                "attributes": "bar",
-            })),
+                "attributes": {},
+            }),
+            "unknown field `attributes`, expected `element`",
+        );
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "Set",
+                "element": {"type": "Long"},
+                "additionalAttributes": false,
+            }),
+            "unknown field `additionalAttributes`, expected `element`",
+        );
+    }
+
+    #[test]
+    fn in_entity() {
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "Entity",
+                "name": "User",
+                "element": {"type": "Long"},
+            }),
+            "unknown field `element`, expected `name`",
+        );
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "Entity",
+                "name": "User",
+                "attributes": {},
+            }),
+            "unknown field `attributes`, expected `name`",
+        );
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "Entity",
+                "name": "User",
+                "additionalAttributes": false,
+            }),
+            "unknown field `additionalAttributes`, expected `name`",
+        );
+    }
+
+    #[test]
+    fn in_extension() {
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "Extension",
+                "name": "decimal",
+                "element": {"type": "Long"},
+            }),
+            "unknown field `element`, expected `name`",
+        );
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "Extension",
+                "name": "decimal",
+                "attributes": {},
+            }),
+            "unknown field `attributes`, expected `name`",
+        );
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "Extension",
+                "name": "decimal",
+                "additionalAttributes": false,
+            }),
+            "unknown field `additionalAttributes`, expected `name`",
+        );
+    }
+
+    #[test]
+    fn in_record() {
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "Record",
+                "attributes": {},
+                "element": {"type": "Long"},
+            }),
+            "unknown field `element`, expected `attributes` or `additionalAttributes`",
+        );
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "Record",
+                "attributes": {},
+                "name": "decimal",
+            }),
+            "unknown field `name`, expected `attributes` or `additionalAttributes`",
+        );
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "Record",
+                "attributes": { "a": {"type": "Long", "name": "foo"} },
+            }),
+            "unknown field `name`, there are no fields",
+        );
+    }
+
+    #[test]
+    fn in_common() {
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "ty",
+                "name": "my_long",
+            }),
+            "unknown field `name`, there are no fields",
+        );
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "ty",
+                "element": {"type": "Long"},
+            }),
+            "unknown field `element`, there are no fields",
+        );
+        assert_ok_deprecated_and_err_standard(
+            json!({
+                "type": "ty",
+                "attributes": {},
+            }),
             "unknown field `attributes`, there are no fields",
         );
         assert_ok_deprecated_and_err_standard(
-            schema_with_attribute(json!({
-                "type": "Long",
-                "element": {"type": "Long"},
-                "additional_attributes": "bar",
-            })),
-            "unknown field `additional_attributes`, there are no fields",
+            json!({
+                "type": "ty",
+                "additionalAttributes": false,
+            }),
+            "unknown field `additionalAttributes`, there are no fields",
         );
     }
+}
+
+/// These tests check the behavior of extra fields that should still be forbidden
+#[cfg(test)]
+#[allow(deprecated)]
+mod extra_fields_forbidden {
+    use cedar_policy_core::test_utils::{expect_err, ExpectedErrorMessageBuilder};
+    use miette::Report;
+    use serde_json::json;
+
+    use crate::Schema;
 
     #[track_caller]
     fn assert_err_deprecated_and_standard(schema: serde_json::Value, err: &str) {
@@ -283,7 +486,7 @@ mod test {
                     "foo": {},
                 }
             }),
-            "unknown field `foo`, expected one of `commonTypes`, `entityTypes`, `actions`",
+            "unknown field `foo`, expected one of `commonTypes`, `entityTypes`, `actions`, `annotations`",
         );
     }
 
@@ -300,7 +503,7 @@ mod test {
                     "actions": {},
                 }
             }),
-            "unknown field `foo`, expected `memberOfTypes` or `shape`",
+            "unknown field `foo`, expected one of `memberOfTypes`, `shape`, `tags`, `enum`, `annotations`",
         );
     }
 
@@ -318,7 +521,7 @@ mod test {
                     },
                 }
             }),
-            "unknown field `foo`, expected one of `attributes`, `appliesTo`, `memberOf`",
+            "unknown field `foo`, expected one of `attributes`, `appliesTo`, `memberOf`, `annotations`",
         );
     }
 
@@ -340,27 +543,6 @@ mod test {
                 }
             }),
             "unknown field `foo`, expected one of `resourceTypes`, `principalTypes`, `context`",
-        );
-    }
-
-    #[test]
-    fn extra_field_in_entity_shape() {
-        assert_err_deprecated_and_standard(
-            json!({
-                "ns": {
-                    "entityTypes": {
-                        "User": {
-                            "shape": {
-                                "type": "Record",
-                                "foo": {},
-                                "attributes": {},
-                            },
-                        }
-                    },
-                    "actions": {},
-                }
-            }),
-            "unknown field `foo`, expected `memberOfTypes` or `shape`",
         );
     }
 }
