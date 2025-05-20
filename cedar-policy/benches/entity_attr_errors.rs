ccca390b1a3435b9754ebee992698248b3a148a3
shaobo-he-aws <130499339+shaobo-he-aws@users.noreply.github.com>
2025-05-20 06:17:21 -0700

    Replace `criterion::black_box` with `std::hint::black_box` (#1630)
    Signed-off-by: Shaobo He <shaobohe@amazon.com>

--- cedar-policy/benches/entity_attr_errors.rs
+++ cedar-policy/benches/entity_attr_errors.rs
@@ -21,9 +21,10 @@
 use cedar_policy::{
     Authorizer, Context, Entities, Entity, EntityUid, PolicySet, Request, RestrictedExpression,
 };
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use std::{
     collections::{HashMap, HashSet},
+    hint::black_box,
     str::FromStr,
 };
 
