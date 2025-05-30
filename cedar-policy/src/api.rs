85eed5439d4770de5fa7ff8d4953f359b06c55f7
Adrian Palacios <accorell@amazon.com>
2025-05-30 11:29:25 -0400

    Actually enable fast version
    Signed-off-by: Adrian Palacios <accorell@amazon.com>

--- cedar-policy/src/api.rs
+++ cedar-policy/src/api.rs
@@ -3718,7 +3718,7 @@ impl Policy {
     /// policies
     #[cfg(feature = "fast-parsing")]
     pub fn parse_lossy(id: Option<PolicyId>, policy_src: impl AsRef<str>) -> Option<Self> {
-        let inline_ast = parser::parse_policy(id.map(Into::into), policy_src.as_ref()).ok()?;
+        let inline_ast = parser::parse_policy_fast(id.map(Into::into), policy_src.as_ref()).ok()?;
         let (_, ast) = ast::Template::link_static_policy(inline_ast);
         Some(Self {
             ast,
