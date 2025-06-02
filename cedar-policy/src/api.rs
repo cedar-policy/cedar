75c0010db0f129ae113ccc16f32c548f5d78fbae
Adrian Palacios <accorell@amazon.com>
2025-06-02 11:17:50 -0400

    Reformat
    Signed-off-by: Adrian Palacios <accorell@amazon.com>

--- cedar-policy/src/api.rs
+++ cedar-policy/src/api.rs
@@ -3762,8 +3762,7 @@ impl Policy {
     /// Only available with the "raw-parsing" feature.
     #[cfg(feature = "raw-parsing")]
     pub fn parse_raw(id: Option<PolicyId>, policy_src: impl AsRef<str>) -> Option<Self> {
-        let inline_ast =
-            parser::parse_policy_raw(id.map(Into::into), policy_src.as_ref()).ok()?;
+        let inline_ast = parser::parse_policy_raw(id.map(Into::into), policy_src.as_ref()).ok()?;
         let (_, ast) = ast::Template::link_static_policy(inline_ast);
         Some(Self {
             ast,
