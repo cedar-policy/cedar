7cf11ae2e7fd8d11f466e15242b78779ed8164c8
Mish Jude <mishiej@amazon.com>
2025-06-12 18:25:35 +0000

    Fix formatting issues + signing off
    Signed-off-by: Mish Jude <mishiej@amazon.com>

--- cedar-policy/src/ffi/check_parse.rs
+++ cedar-policy/src/ffi/check_parse.rs
@@ -130,6 +130,7 @@ pub fn check_parse_scope_variables(call: ScopeVariablesParsingCall) -> CheckPars
         }
     };
 
+    //all three should be provided, so just call validate
     if let Err(err) =
         crate::api::validate_scope_variables(&principal, &action, &resource, &schema_ref)
     {
