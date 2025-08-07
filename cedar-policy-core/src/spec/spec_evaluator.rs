b943493d01b44b41d629f01a7edbce9a433d6d37
Pratap Singh <sgpratap@amazon.com>
2025-08-07 20:28:25 +0000

    strengthen lemma_evaluate_to_expr_left_assoc_equal to also account for error cases

--- cedar-policy-core/src/spec/spec_evaluator.rs
+++ cedar-policy-core/src/spec/spec_evaluator.rs
@@ -593,9 +593,16 @@ pub proof fn lemma_eval_and_spec(a: Expr, b: Expr, req: Request, es: Entities, s
         match (evaluate(a, req, es, slot_env), evaluate(b, req, es, slot_env)) {
             (Ok(Value::Prim { p: Prim::Bool { b: bool_a } }), Ok(Value::Prim { p: Prim::Bool { b: bool_b } })) =>
                 evaluate(Expr::and(a, b), req, es, slot_env) matches Ok(Value::Prim { p: Prim::Bool { b } }) && b == (bool_a && bool_b),
-            (Ok(Value::Prim { p: Prim::Bool { b: bool_a } }), _) => !bool_a ==>
-                (evaluate(Expr::and(a, b), req, es, slot_env) matches Ok(Value::Prim { p: Prim::Bool { b } }) && b == false),
-            (_, _) => evaluate(Expr::and(a, b), req, es, slot_env) is Err
+            (Ok(Value::Prim { p: Prim::Bool { b: bool_a } }), Ok(_)) => {
+                &&& !bool_a ==> (evaluate(Expr::and(a, b), req, es, slot_env) matches Ok(Value::Prim { p: Prim::Bool { b } }) && b == false)
+                &&& bool_a ==> (evaluate(Expr::and(a, b), req, es, slot_env) matches Err(e) && e == Error::TypeError)
+            },
+            (Ok(Value::Prim { p: Prim::Bool { b: bool_a } }), Err(e2)) => {
+                &&& !bool_a ==> (evaluate(Expr::and(a, b), req, es, slot_env) matches Ok(Value::Prim { p: Prim::Bool { b } }) && b == false)
+                &&& bool_a ==> (evaluate(Expr::and(a, b), req, es, slot_env) matches Err(e) && e == e2)
+            },
+            (Ok(_), _) => evaluate(Expr::and(a, b), req, es, slot_env) matches Err(e) && e == Error::TypeError,
+            (Err(e1), _) => evaluate(Expr::and(a, b), req, es, slot_env) matches Err(e) && e == e1,
         }
     })
 {
@@ -604,10 +611,7 @@ pub proof fn lemma_eval_and_spec(a: Expr, b: Expr, req: Request, es: Entities, s
 
 pub proof fn lemma_eval_and_assoc(a: Expr, b: Expr, c: Expr, req: Request, es: Entities, slot_env: SlotEnv)
     ensures
-        evaluate(Expr::and(a, Expr::and(b, c)), req, es, slot_env) matches Ok(v1) ==>
-                evaluate(Expr::and(Expr::and(a, b), c), req, es, slot_env) matches Ok(v2) && v1 == v2,
-        evaluate(Expr::and(a, Expr::and(b, c)), req, es, slot_env) is Err ==>
-                evaluate(Expr::and(Expr::and(a, b), c), req, es, slot_env) is Err
+        evaluate(Expr::and(a, Expr::and(b, c)), req, es, slot_env) == evaluate(Expr::and(Expr::and(a, b), c), req, es, slot_env)
 {
     reveal_with_fuel(evaluate, 1);
     lemma_eval_and_spec(a, Expr::and(b, c), req, es, slot_env);
@@ -616,10 +620,7 @@ pub proof fn lemma_eval_and_assoc(a: Expr, b: Expr, c: Expr, req: Request, es: E
 
 pub proof fn lemma_evaluate_to_expr_left_assoc_equal(p: Policy, req: Request, entities: Entities)
     ensures
-        evaluate(p.to_expr(), req, entities, p.slot_env) matches Ok(v) ==>
-                evaluate(p.to_expr_left_assoc(), req, entities, p.slot_env) matches Ok(v_left) && v == v_left,
-        evaluate(p.to_expr(), req, entities, p.slot_env) is Err ==>
-                evaluate(p.to_expr_left_assoc(), req, entities, p.slot_env) is Err
+        evaluate(p.to_expr(), req, entities, p.slot_env) == evaluate(p.to_expr_left_assoc(), req, entities, p.slot_env)
 {
     reveal(Policy::to_expr);
     reveal(Policy::to_expr_left_assoc);
