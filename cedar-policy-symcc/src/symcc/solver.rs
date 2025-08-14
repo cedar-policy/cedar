2f25a67ee3d90a9685f8f3587e8bcbf4c399e8be
Shaobo He <shaobohe@amazon.com>
2025-08-14 15:04:57 -0700

    Add a dummy solver that inspects SMT-LIB generated
    Co-authored-by: Charlie Murphy <mutmoth@amazon.com>
    
    Signed-off-by: Shaobo He <shaobohe@amazon.com>

--- cedar-policy-symcc/src/symcc/solver.rs
+++ cedar-policy-symcc/src/symcc/solver.rs
@@ -218,6 +218,62 @@ impl<W: tokio::io::AsyncWrite + Unpin + Send> Solver for WriterSolver<W> {
     }
 }
 
+/// An implementation of [`Solver`] that stores the SMTLib text in a buffer
+/// and allows the use of the solver to take the contents of the buffer
+#[derive(Debug)]
+pub struct BuffSolver(WriterSolver<tokio::io::BufWriter<Vec<u8>>>);
+
+impl BuffSolver {
+    /// Create a [`BuffSolver`]
+    pub fn new() -> Self {
+        Self(WriterSolver::<tokio::io::BufWriter<Vec<u8>>> {
+            w: tokio::io::BufWriter::new(Vec::new()),
+        })
+    }
+
+    /// Obtain the contents of the buffer
+    pub fn contents(&mut self) -> String {
+        let buffer = self.0.w.get_mut();
+        // PANIC SAFETY: contents written into the buffer should always be utf8-encoded strings
+        #[allow(clippy::expect_used)]
+        let ret = String::from_utf8(buffer.clone())
+            .expect("Unexpected error converting Vec<u8> to String");
+        buffer.clear();
+        ret
+    }
+}
+
+impl Solver for BuffSolver {
+    fn smtlib_input(&mut self) -> &mut (dyn tokio::io::AsyncWrite + Unpin + Send) {
+        self.0.smtlib_input()
+    }
+
+    async fn check_sat(&mut self) -> Result<Decision> {
+        self.0.check_sat().await
+    }
+
+    async fn get_model(&mut self) -> Result<Option<String>> {
+        self.0.get_model().await
+    }
+}
+
+#[cfg(test)]
+mod buff_solver_tests {
+    use crate::{solver::Solver, SmtLibScript};
+
+    #[tokio::test]
+    async fn test_buff_solver() {
+        let mut solver = super::BuffSolver::new();
+        solver.smtlib_input().assert("false").await.unwrap();
+        let decision = solver.check_sat().await.unwrap();
+        assert_eq!(decision, super::Decision::Unknown);
+        let model = solver.get_model().await.unwrap();
+        assert!(model.is_none());
+        let contents = solver.contents();
+        assert_eq!(contents, "(assert false)\n(check-sat)\n(get-model)\n");
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
