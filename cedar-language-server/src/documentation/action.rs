b8cef2591accedf91ac8f2d03f12a551f330c527
John Kastner <jkastner@amazon.com>
2025-09-09 16:59:37 +0000

    Add action documentation tests

--- cedar-language-server/src/documentation/action.rs
+++ cedar-language-server/src/documentation/action.rs
@@ -58,7 +58,7 @@ impl ToDocumentationString for ActionDocumentation<'_> {
             return static_docs.into();
         };
         let mut builder = MarkdownBuilder::new();
-        builder.push_str(static_docs);
+        builder.push_with_new_line(static_docs);
 
         match constraint {
             ActionConstraint::Any => {
@@ -129,5 +129,199 @@ fn format_action_list<'a>(
 
     if remaining > 0 {
         builder.paragraph(&format!("*... and {remaining} more actions defined*"));
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use cedar_policy_core::ast::{ActionConstraint, EntityUID};
+    use cedar_policy_core::validator::ValidatorSchema;
+    use insta::assert_snapshot;
+    use std::sync::Arc;
+
+    use super::*;
+
+    fn test_schema() -> ValidatorSchema {
+        r#"
+          entity Photo;
+          entity User {
+            name: String,
+            age: Long
+          };
+
+          action Read appliesTo {
+             principal: User,
+             resource: Photo,
+             context: {
+                authenticated: Bool,
+                request: {
+                   timestamp: datetime
+                }
+             }
+          };
+          
+          action Write appliesTo {
+             principal: User,
+             resource: Photo,
+             context: {
+                ip_address: String,
+                request: {
+                   path: String
+                }
+             }
+          };
+
+          action Delete, View, Edit, Create, Update, List, Search, Download, Upload, Share appliesTo {
+             principal: User,
+             resource: Photo
+          };
+        "#
+        .parse()
+        .unwrap()
+    }
+
+    #[test]
+    fn test_action_documentation_default_no_schema() {
+        let action_doc = ActionDocumentation::new(None);
+        assert_snapshot!(action_doc.to_documentation_string(None));
+    }
+
+    #[test]
+    fn test_action_documentation_any_no_schema() {
+        let constraint = ActionConstraint::Any;
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        assert_snapshot!(action_doc.to_documentation_string(None));
+    }
+
+    #[test]
+    fn test_action_documentation_any_with_schema() {
+        let constraint = ActionConstraint::Any;
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        let schema = test_schema();
+        assert_snapshot!(action_doc.to_documentation_string(Some(&schema)));
+    }
+
+    #[test]
+    fn test_action_documentation_eq_no_schema() {
+        let action_euid: EntityUID = "Action::\"Read\"".parse().unwrap();
+        let constraint = ActionConstraint::Eq(Arc::new(action_euid));
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        assert_snapshot!(action_doc.to_documentation_string(None));
+    }
+
+    #[test]
+    fn test_action_documentation_eq_with_schema() {
+        let action_euid: EntityUID = "Action::\"Read\"".parse().unwrap();
+        let constraint = ActionConstraint::Eq(Arc::new(action_euid));
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        let schema = test_schema();
+        assert_snapshot!(action_doc.to_documentation_string(Some(&schema)));
+    }
+
+    #[test]
+    fn test_action_documentation_eq_no_context_with_schema() {
+        let action_euid: EntityUID = "Action::\"Delete\"".parse().unwrap();
+        let constraint = ActionConstraint::Eq(Arc::new(action_euid));
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        let schema = test_schema();
+        assert_snapshot!(action_doc.to_documentation_string(Some(&schema)));
+    }
+
+    #[test]
+    fn test_action_documentation_in_empty_no_schema() {
+        let actions = Vec::new();
+        let constraint = ActionConstraint::In(actions);
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        assert_snapshot!(action_doc.to_documentation_string(None));
+    }
+
+    #[test]
+    fn test_action_documentation_in_empty_with_schema() {
+        let actions = Vec::new();
+        let constraint = ActionConstraint::In(actions);
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        let schema = test_schema();
+        assert_snapshot!(action_doc.to_documentation_string(Some(&schema)));
+    }
+
+    #[test]
+    fn test_action_documentation_in_single_no_schema() {
+        let action_euid: EntityUID = "Action::\"Read\"".parse().unwrap();
+        let actions = vec![Arc::new(action_euid)];
+        let constraint = ActionConstraint::In(actions);
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        assert_snapshot!(action_doc.to_documentation_string(None));
+    }
+
+    #[test]
+    fn test_action_documentation_in_single_with_schema() {
+        let action_euid: EntityUID = "Action::\"Read\"".parse().unwrap();
+        let actions = vec![Arc::new(action_euid)];
+        let constraint = ActionConstraint::In(actions);
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        let schema = test_schema();
+        assert_snapshot!(action_doc.to_documentation_string(Some(&schema)));
+    }
+
+    #[test]
+    fn test_action_documentation_in_multiple_no_schema() {
+        let read_euid: EntityUID = "Action::\"Read\"".parse().unwrap();
+        let write_euid: EntityUID = "Action::\"Write\"".parse().unwrap();
+        let delete_euid: EntityUID = "Action::\"Delete\"".parse().unwrap();
+        let actions = vec![
+            Arc::new(read_euid),
+            Arc::new(write_euid),
+            Arc::new(delete_euid),
+        ];
+        let constraint = ActionConstraint::In(actions);
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        assert_snapshot!(action_doc.to_documentation_string(None));
+    }
+
+    #[test]
+    fn test_action_documentation_in_multiple_with_schema() {
+        let read_euid: EntityUID = "Action::\"Read\"".parse().unwrap();
+        let write_euid: EntityUID = "Action::\"Write\"".parse().unwrap();
+        let delete_euid: EntityUID = "Action::\"Delete\"".parse().unwrap();
+        let actions = vec![
+            Arc::new(read_euid),
+            Arc::new(write_euid),
+            Arc::new(delete_euid),
+        ];
+        let constraint = ActionConstraint::In(actions);
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        let schema = test_schema();
+        assert_snapshot!(action_doc.to_documentation_string(Some(&schema)));
+    }
+
+    #[test]
+    fn test_action_documentation_in_many_actions_with_schema() {
+        // Test the truncation behavior with more than MAX_ACTIONS_TO_SHOW (10) actions
+        let actions: Vec<Arc<EntityUID>> = [
+            "Read", "Write", "Delete", "View", "Edit", "Create", "Update", "List", "Search",
+            "Download", "Upload", "Share",
+        ]
+        .iter()
+        .map(|name| Arc::new(format!("Action::\"{name}\"").parse().unwrap()))
+        .collect();
+        let constraint = ActionConstraint::In(actions);
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        let schema = test_schema();
+        assert_snapshot!(action_doc.to_documentation_string(Some(&schema)));
+    }
+
+    #[test]
+    fn test_action_documentation_error_constraint_no_schema() {
+        let constraint = ActionConstraint::ErrorConstraint;
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        assert_snapshot!(action_doc.to_documentation_string(None));
+    }
+
+    #[test]
+    fn test_action_documentation_error_constraint_with_schema() {
+        let constraint = ActionConstraint::ErrorConstraint;
+        let action_doc = ActionDocumentation::new(Some(&constraint));
+        let schema = test_schema();
+        assert_snapshot!(action_doc.to_documentation_string(Some(&schema)));
     }
 }
