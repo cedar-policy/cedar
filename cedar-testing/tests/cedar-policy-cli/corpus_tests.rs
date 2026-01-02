e12e5aeb167bab2e9a4b72d3fe16ea8bf079a055
John Kastner <jkastner@amazon.com>
2026-01-02 16:42:33 +0000

    Add test utility for iterating over corpus tests
    Signed-off-by: John Kastner <jkastner@amazon.com>

--- cedar-testing/tests/cedar-policy-cli/corpus_tests.rs
+++ cedar-testing/tests/cedar-policy-cli/corpus_tests.rs
@@ -17,14 +17,8 @@
 //! Integration tests auto-generated using the differential tester.
 
 use super::perform_integration_test_from_json;
-use super::resolve_integration_test_path;
+use cedar_testing::test_files::get_corpus_tests;
 use rstest::rstest;
-use std::path::Path;
-
-/// Path of the folder containing the corpus tests
-fn folder() -> &'static Path {
-    Path::new("corpus-tests")
-}
 
 // for now we have a single #[test] that runs all the corpus tests.
 // The disadvantage of this is that only one error message will be displayed,
@@ -49,26 +43,7 @@ fn corpus_tests(
     )]
     prefix: &str,
 ) {
-    let corpus_tests_folder = resolve_integration_test_path(folder());
-    let test_jsons = std::fs::read_dir(&corpus_tests_folder)
-        .unwrap_or_else(|e| {
-            panic!(
-                "failed to read corpus-tests folder {}: {e}",
-                corpus_tests_folder.display()
-            )
-        })
-        .map(|e| e.expect("failed to access file in corpus-tests").path())
-        .filter(|p| {
-            let filename = p
-                .file_name()
-                .expect("didn't expect subdirectories in corpus-tests")
-                .to_str()
-                .expect("expected filenames to be valid UTF-8");
-            filename.starts_with(prefix)
-                && filename.ends_with(".json")
-                && !filename.ends_with(".entities.json")
-        });
-    for test_json in test_jsons {
+    for test_json in get_corpus_tests(prefix) {
         println!("testing {}", test_json.display());
         perform_integration_test_from_json(test_json);
     }
